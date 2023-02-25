cd0fef29bfbce78fdf6c4bdcf506493a0a6ea41f
brunokian <brunokian@hotmail.com>
2023-02-25 20:24:12 -0300

    teste

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom"
 import InitialPage from "./pages/InitialPage";
 import CarPage from "./pages/CarPage";
 
+
 function App() {
   return (
     <Routes>
